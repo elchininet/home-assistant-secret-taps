fed09f12a420e7a0a7fecb0c0793e95ca72391c7
Iván Pereira Muñoz <elchininet@gmail.com>
2024-11-10 14:32:42 +0100

    Remove getPromisableElement utility

--- src/checker.ts
+++ src/checker.ts
@@ -1,34 +1,14 @@
+import { getPromisableResult } from 'get-promisable-result';
 import { NAMESPACE } from '@constants';
 
-export const MAX_ATTEMPTS = 100;
-export const RETRY_DELAY = 50;
-
-const getPromisableElement = <T>(
-    getElement: () => T,
-    check: (element: T) => boolean
-): Promise<T> => {
-    return new Promise<T>((resolve) => {
-        let attempts = 0;
-        const select = () => {
-            const element: T = getElement();
-            if (element && check(element)) {
-                resolve(element);
-            } else {
-                attempts++;
-                if (attempts < MAX_ATTEMPTS) {
-                    setTimeout(select, RETRY_DELAY);
-                } else {
-                    resolve(element);
-                }
-            }
-        };
-        select();
-    });
-};
-
-getPromisableElement(
+getPromisableResult(
     () => window.HomeAssistantSecretTaps,
-    (module: object) => !!module
+    (module: object) => !!module,
+    {
+        retries: 100,
+        delay: 50,
+        shouldReject: false
+    }
 ).then((module) => {
     if (!module) {
         throw Error(`${NAMESPACE}: you need to add the plugin as a frontend > extra_module_url module.\nCheck the documentation: https://github.com/elchininet/home-assistant-secret-taps#installation`);
